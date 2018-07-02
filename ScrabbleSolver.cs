5d3cd84ca8054d196246cb02cb4042eda7603a1e
Zack M Fleischman <ZackMFleischman@gmail.com>
2018-07-02 16:39:40 -0700

    Adding comments

--- ScrabbleSolver.cs
+++ ScrabbleSolver.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.IO;
 
+// This program should output all valid Scrabble words formed from the 7 letters given as input.
+//
+// The 7 letters will be provided as a single string as the first command line parameter.
+// They are in the range [a-z] and can be assumed to be valid input (always 7, and always lowercase letters from 'a' to 'z') 
 class ScrabbleSolver {
     static void Main(string[] args) {
-        string letters = args[0]; // You may assume this is a string of 7 characters in the range [a-z].
+        string letters = args[0];
         string[] allScrabbleWords = File.ReadAllLines("ScrabbleDictionary.txt");
         
         string[] validScrabbleWordsFormedWithLetters = GetValidScrabbleWordsFromLetters(letters, allScrabbleWords);
