456e79eb8711282e3e9bcc618c392cf32459b18a
Zack M Fleischman <ZackMFleischman@gmail.com>
2018-07-02 16:31:59 -0700

    Style refactor

--- ScrabbleSolver.cs
+++ ScrabbleSolver.cs
@@ -4,14 +4,11 @@ using System.IO;
 class ScrabbleSolver {
     static void Main(string[] args) {
         string letters = args[0]; // You may assume this is a string of 7 characters in the range [a-z].
-        string[] allScrabbleWords = _LoadScrabbleDictionary();
+        string[] allScrabbleWords = File.ReadAllLines("ScrabbleDictionary.txt");
         
         string[] validScrabbleWordsFormedWithLetters = _GetValidScrabbleWordsFromLetters(letters, allScrabbleWords);
-
         _DumpValidScrabbleWordsToConsole(validScrabbleWordsFormedWithLetters, letters);
     }
-
-    static string[] _LoadScrabbleDictionary() => File.ReadAllLines("ScrabbleDictionary.txt");
 
     static void _DumpValidScrabbleWordsToConsole(string[] scrabbleWords, string lettersUsed) {
         Console.WriteLine($"Found {scrabbleWords.Length} valid Scrabble words formed with the letters '{lettersUsed}':");
