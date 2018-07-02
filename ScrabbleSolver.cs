cbf6b43f9a3bdcd57426c10319c40bb1a26d72e1
Zack M Fleischman <ZackMFleischman@gmail.com>
2018-07-02 16:35:11 -0700

    More style nits

--- ScrabbleSolver.cs
+++ ScrabbleSolver.cs
@@ -6,11 +6,11 @@ class ScrabbleSolver {
         string letters = args[0]; // You may assume this is a string of 7 characters in the range [a-z].
         string[] allScrabbleWords = File.ReadAllLines("ScrabbleDictionary.txt");
         
-        string[] validScrabbleWordsFormedWithLetters = _GetValidScrabbleWordsFromLetters(letters, allScrabbleWords);
-        _DumpValidScrabbleWordsToConsole(validScrabbleWordsFormedWithLetters, letters);
+        string[] validScrabbleWordsFormedWithLetters = GetValidScrabbleWordsFromLetters(letters, allScrabbleWords);
+        DumpValidScrabbleWordsToConsole(validScrabbleWordsFormedWithLetters, letters);
     }
 
-    static void _DumpValidScrabbleWordsToConsole(string[] scrabbleWords, string lettersUsed) {
+    static void DumpValidScrabbleWordsToConsole(string[] scrabbleWords, string lettersUsed) {
         Console.WriteLine($"Found {scrabbleWords.Length} valid Scrabble words formed with the letters '{lettersUsed}':");
         foreach (string word in scrabbleWords)
             Console.WriteLine(word);
@@ -19,7 +19,7 @@ class ScrabbleSolver {
     //
     // Please implement this function.
     //
-    static string[] _GetValidScrabbleWordsFromLetters(string letters, string[] allScrabbleWords) {
+    static string[] GetValidScrabbleWordsFromLetters(string letters, string[] allScrabbleWords) {
         return new string[0];
     }
 }
